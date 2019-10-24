e0b72147d4cb051ad12cfa021825875237a32b15
Kumar Abhirup <kumar@iqubex.com>
2019-10-24 14:48:15 +0530

    🐛 FIX: Fix the malfunctioning Slingshot after it goes out of the screen

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -624,6 +624,16 @@ function keyPressed() {
         )
       }
     }
+
+    if (key === ' ') {
+      shootingPig.destruct()
+      shootingPig = new GameObject(
+        { x: width / 2, y: height / 2 },
+        { radius: 1 * objSize },
+        { shape: 'circle', color: '#ffff00', movable: true, rotate: true }
+      )
+      slingshot.attach(shootingPig.body)
+    }
   }
 
   // After quitting
