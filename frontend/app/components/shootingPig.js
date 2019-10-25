734d76e61657e1d4776089edd79788f180d9df9e
Kumar Abhirup <kumar@iqubex.com>
2019-10-25 13:08:52 +0530

    📦 NEW: Create shootingPig and change it's density

--- frontend/app/components/shootingPig.js
+++ frontend/app/components/shootingPig.js
@@ -5,6 +5,18 @@
 
   Koji
   GameObject
+  Matter
 */
 
-class ShootingPig extends GameObject {}
+const { Body } = Matter
+
+class ShootingPig extends GameObject {
+  constructor(cordinates, sizing, settings) {
+    super(cordinates, sizing, settings)
+
+    this.body.positionPrev.x = this.body.position.x
+    this.body.positionPrev.y = this.body.position.y
+
+    Body.setDensity(this.body, 99999999)
+  }
+}
